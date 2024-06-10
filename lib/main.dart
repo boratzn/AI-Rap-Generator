4bdf4211ce2d4e58ba2817c7cde86eb72f1d1ac0
boratzn <boratzn94@gmail.com>
2024-06-10 10:15:59 +0300

    navigation key added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:ai_rap_generator/feature/home/view/home_view.dart';
 import 'package:ai_rap_generator/feature/index.dart';
 import 'package:ai_rap_generator/feature/onboarding/viewmodel/onboarding_viewmodel.dart';
-import 'package:ai_rap_generator/product/blocs/beats/beats_bloc.dart';
-import 'package:ai_rap_generator/product/blocs/hive/hive_bloc.dart';
-import 'package:ai_rap_generator/product/blocs/lyric/lyric_bloc.dart';
-import 'package:ai_rap_generator/product/blocs/rapper/rapper_bloc.dart';
-import 'package:ai_rap_generator/product/blocs/song/song_bloc.dart';
+import 'package:ai_rap_generator/product/navigation/navigation_service.dart';
 import 'package:ai_rap_generator/product/providers/lyric_provider.dart';
-import 'package:ai_rap_generator/product/providers/prompt/timer_provider.dart';
 import 'package:ai_rap_generator/product/providers/song_creator/index_provider.dart';
 import 'package:ai_rap_generator/product/service/database_service/hive_service.dart';
 import 'package:flutter/material.dart';
@@ -67,6 +61,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: Navigation.navigationKey,
       debugShowCheckedModeBanner: false,
       home: seenOnboarding ? const HomePageView() : const OnBoardingView(),
     );
